011cd7bf112855a1498a2773e5f6f841f05f4f76
Ben Crowder <ben.crowder@gmail.com>
2011-01-11 13:34:19 -0700

    Moved CSS and JS into their own directories

--- include/header.php
+++ include/header.php
@@ -4,11 +4,11 @@
 	<meta charset="UTF-8">
 	<title>Unbindery</title>
 
-	<link rel="stylesheet" href="<?php echo $SITEROOT; ?>/unbindery.css" type="text/css" media="screen" charset="utf-8" />
+	<link rel="stylesheet" href="<?php echo $SITEROOT; ?>/css/unbindery.css" type="text/css" media="screen" charset="utf-8" />
 
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo $SITEROOT; ?>/lib/jquery.hotkeys.js"></script>
-	<script type="text/javascript" src="<?php echo $SITEROOT; ?>/unbindery.js"></script>
+	<script type="text/javascript" src="<?php echo $SITEROOT; ?>/js/unbindery.js"></script>
 </head>
 <body>
 	<div id="header_container">
