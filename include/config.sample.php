d9b6303362b9e819c21c4c5187354b345f9d40a7
Ben Crowder <ben.crowder@gmail.com>
2011-03-01 10:33:55 -0700

    Split guidelines out into separate file

--- include/config.sample.php
+++ include/config.sample.php
@@ -9,12 +9,6 @@ $SITEROOT = "http://mysite.com";
 
 $ADMINEMAIL = "your@email.com";
 
-$SYSTEMGUIDELINES = <<<EOG;
-<i>Questions? Email your administrator (your@email.com)</i>
-
-<h3>General</h3>
-
-<p>Follow <a href='http://www.gutenberg.org/wiki/Gutenberg:Volunteers%27_FAQ#About_the_formatting_of_a_text_file'>the Project Gutenberg guidelines</a>.</p>
-EOG;
+include_once("guidelines.php");
 
 ?>
