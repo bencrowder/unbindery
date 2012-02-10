29eb620e11cdd94c0050e757b6c86de6688197b8
bencrowder <ben.crowder@gmail.com>
2012-02-09 20:06:08 -0700

    Added test for multiple hooks on a single event to the events module

--- tests/event-test.php
+++ tests/event-test.php
@@ -12,6 +12,9 @@ print_r($events);
 echo "\nRegistering an 'echo'/'system' event...\n";
 $eventManager->register("echo", "system", "myEvent");
 
+echo "\nRegistering an 'echo'/'system' event...\n";
+$eventManager->register("echo", "system", "myEvent2");
+
 echo "\nRegistering a 'script'/'project' event...\n";
 $eventManager->register("script", "project", "Test::event");
 
@@ -21,9 +24,18 @@ echo "Events: \n";
 print_r($events);
 
 echo "\nTriggering said event with 'apple' and 'water' passed...\n";
+echo "Also triggering event 2...\n";
 $eventManager->trigger("echo", "system", array('fruit' => 'apple', 'beverage' => 'water'));
 
 echo "\nRemoving said event...\n";
+$eventManager->remove("echo", "system", "myEvent");
+
+echo "\nGetting events for 'system' group...\n";
+$events = $eventManager->getEventsByGroup('system');
+echo "Events: \n";
+print_r($events);
+
+echo "\nRemoving all echo event...\n";
 $eventManager->remove("echo", "system");
 
 echo "\nGetting events for 'system' group...\n";
@@ -35,12 +47,15 @@ echo "\nTriggering 'script' event with 'hck' passed...\n";
 $eventManager->trigger("script", "project", array('project' => 'hck'));
 
 
-
 function myEvent($params) {
 	$fruit = $params['fruit'];
 	$beverage = $params['beverage'];
 
 	echo "\n** Hello, myEvent was called and $fruit was passed, with a drink of $beverage on the side.\n";
+}
+
+function myEvent2($params) {
+	echo "\n** Second, success.\n";
 }
 
 class Test {
