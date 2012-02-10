7f4d6adeb78c1d8de1be3c1a5d8ebb05bc3151ad
bencrowder <ben.crowder@gmail.com>
2012-02-09 20:56:29 -0700

    Added i18n module

---
+++ modules/I18n.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once '../lib/sfyaml/sfYaml.php';
+
+class I18n {
+	private $translations = array();
+
+
+	// Constructor
+	// --------------------------------------------------
+
+	public function I18n($locale='en') {
+		// Default to en.php if the locale doesn't exist
+		$filename = "../translations/en.yaml";
+		if (file_exists("../translations/$locale.yaml")) {
+			$filename = "../translations/$locale.yaml";
+		}
+
+		// Load the YAML file
+		$this->translations = sfYaml::load($filename);
+	}
+
+
+	// Translate
+	// --------------------------------------------------
+
+	public function translate($key) {
+		if (isset($this->translations[$key])) {
+			return $this->translations[$key];
+		} else {
+			// TODO: throw error
+			return '[MISSING TRANSLATION]';
+		}
+	}
+}
+
+?>
