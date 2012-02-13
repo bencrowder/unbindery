44413fadb7154da6307bb44f217d6b9425bbcacc
bencrowder <ben.crowder@gmail.com>
2012-02-13 13:26:17 -0700

    Added queue module

---
+++ modules/Queue.php
@@ -0,0 +1,191 @@
+<?php
+
+class Queue {
+	private $queue;
+	private $queueName;
+	private $saveOnChanges;
+	static private $functions = array('compare' => 'Queue::defaultItemCompareFunction');
+
+
+	// Constructor: loads the specified queue
+	//   $saveOnChanges: specifies whether the queue should be saved to the
+	//     data source after each change or not (default = false)
+	// --------------------------------------------------
+
+	public function __construct($queueName = '', $saveOnChanges = false) {
+		$this->saveOnChanges = $saveOnChanges;
+		$this->queue = array();
+		$this->queueName = trim($queueName);
+		$this->load($this->queueName);
+	}
+
+
+	// exists: determines if a function to be registered actually exists
+	// --------------------------------------------------
+
+	static private function exists($function) {
+		$rtn = false;
+
+		if (is_array($function) && count($function) > 1 && method_exists($function[0], $function[1])) {
+			$rtn = true;
+		} elseif (is_string($function) && trim($function) != '' && function_exists($function)) {
+			$rtn = true;
+		}
+
+		return $rtn;
+	}
+
+
+	// save: delegates to the save function specified
+	// --------------------------------------------------
+
+	public function save() {
+		$rtn = false;
+
+		if (!array_key_exists('save', self::$functions)) {
+			return $rtn;
+		}
+
+		if ($this->queueName != '' && self::$functions['save'] != null) {
+			$this->queue = call_user_func(self::$functions['save'], $this->queueName, $this->queue);
+			$rtn = true;
+		}
+
+		return $rtn;
+	}
+
+
+	// load: delegates to the load function specified
+	//   $saveOnChanges: specifies whether the queue should be saved to the
+	//     data source after each change or not (default = false)
+	// --------------------------------------------------
+
+	public function load($queueName, $saveOnChanges = false) {
+		$rtn = false;
+
+		if (!array_key_exists('load', self::$functions)) {
+			return $rtn;
+		}
+
+		if ($queueName != '' && self::$functions['load'] != null) {
+			$this->queue = call_user_func(self::$functions['load'], $queueName);
+			$this->saveOnChanges = $saveOnChanges;
+			$rtn = true;
+		}
+
+		return $rtn;
+	}
+
+
+	// register: registers a function
+	// --------------------------------------------------
+
+	static public function register($action, $function) {
+		$rtn = false;
+
+		if (!self::exists($function)) {
+			throw new Exception('Function does not exist.');
+		} else {
+			self::$functions[$action] = $function;
+			$rtn = true;
+		}
+
+		return $rtn;
+	}
+
+
+	// add: adds an item to the queue
+	// --------------------------------------------------
+
+	public function add($item) {
+		array_push($this->queue, $item);
+
+		if ($this->saveOnChanges) $this->save();
+	}
+
+
+	// remove: finds the specified item and removes it from the queue
+	//   Uses the itemCompareFunction registered
+	// --------------------------------------------------
+
+	public function remove($item) {
+		$rtnItem = null;
+
+		foreach ($this->queue as $key=>$currentItem) {
+			if (call_user_func(self::$functions['compare'], $item, $currentItem)) {
+				$rtnItem = $currentItem;
+				unset($this->queue[$key]);
+				array_filter($this->queue);
+				break;
+			}
+		}
+
+		if ($this->saveOnChanges) $this->save();
+
+		return $rtnItem;
+	}
+
+
+	// getFirstItem: returns the first item in the queue and removes it from the queue
+	// --------------------------------------------------
+
+	public function getFirstItem() {
+		$item = array_shift($this->queue);
+
+		if ($this->saveOnChanges) $this->save();
+
+		return $item;
+	}
+
+
+	// getItems: returns the queue array
+	// --------------------------------------------------
+
+	public function getItems() {
+		return $this->queue;
+	}
+
+
+	// setItems: sets the queue to the $queue array specified
+	// --------------------------------------------------
+
+	public function setItems($queue) {
+		$this->queue = $queue;
+
+		if ($this->saveOnChanges) $this->save();
+	}
+
+
+	// destroy: deletes a queue
+	// --------------------------------------------------
+
+	public function destroy() {
+		$rtn = false;
+
+		if (!array_key_exists('destroy', self::$functions)) {
+			return $rtn;
+		}
+
+		if ($this->queueName != '' && self::$functions['destroy'] != null) {
+			$this->queue = call_user_func(self::$functions['destroy'], $this->queueName);
+			$rtn = true;
+		}
+
+		if ($rtn == true) {
+			unset($this->queue);
+			$this->queueName = '';
+		}
+
+		return $rtn;
+	}
+
+
+	// Default item compare function
+	// --------------------------------------------------
+
+	public static function defaultItemCompareFunction($item1, $item2) {
+		if ($item1 === $item2) return true;
+		else return false;
+	}
+}
+?>
---
+++ tests/queue-test.php
@@ -0,0 +1,114 @@
+<?php
+
+include '../modules/Queue.php';
+
+class Point {
+	public $x;
+	public $y;
+
+	function __construct($x, $y) {
+		$this->x = $x;
+		$this->y = $y;
+	}
+
+	static public function compareItems($item1, $item2) {
+		if ($item1->x == $item2->x) return true;
+		else return false;
+	}
+
+	public function __toString() {
+		return '[' . $this->x . ',' . $this->y . ']';
+	}
+}
+
+$_SERVER['array1'] = array('one', 'two', 'three', 'four');
+$_SERVER['array2'] = array(new Point(1,1), new Point(2,2), new Point(3,3), new Point(4,1));
+
+function destroyQueue($name) {
+	echo "queue destroy\n";
+
+	if ($name == '1') {
+		unset($_SERVER['array1']);
+		return true;
+	} elseif ($name =='2') {
+		unset($_SERVER['array2']);
+		return true;
+	}
+
+	return false;
+}
+
+function saveQueue($name, $array) {
+	echo "saving $name - " . json_encode($array);
+
+	if ($name == '1') {
+		$_SERVER['array1'] = $array;
+		return $_SERVER['array1'];
+	} elseif ($name =='2') {
+		$_SERVER['array2'] = $array;
+		return $_SERVER['array2'];
+	}
+}
+
+function loadQueue($name) {
+	$rtn = array();
+	if ($name == '1') {
+		$rtn = $_SERVER['array1'];
+	} elseif ($name == '2') {
+		$rtn = $_SERVER['array2'];
+	}
+	echo "loaded $name\n";
+	return $rtn;
+}
+
+try {
+	Queue::register('save', 'saveQueue');
+	Queue::register('load', 'loadQueue');
+} catch (Exception $e) {
+	echo $e->getMessage();
+}
+
+$queue = new Queue('1');
+echo "queue 1 loaded\n";
+$item = $queue->getFirstItem();
+echo 'first item: ' . $item . "\n";
+$item = $queue->remove('three');
+echo 'third item: ' . $item . "\n";
+$item = $queue->getFirstItem();
+echo 'second item: ' . $item . "\n";
+if ($queue->save()) echo "\nsaved.\n";
+echo json_encode($queue->getItems());
+echo "\n";
+echo "test 1 done\n\n";
+
+try {
+	Queue::register('compare', array('Point', 'compareItems'));
+	Queue::register('destroy', 'destroyQueue');
+} catch (Exception $e) {
+	echo "FAIL\n";
+	echo $e->getMessage();
+}
+
+$queue = new Queue('2');
+echo "queue 1 loaded\n";
+$item = $queue->getFirstItem();
+echo 'first item: ' . $item . "\n";
+$item = $queue->remove(new Point(4,0));
+echo 'fourth item: ' . $item . "\n";
+$item = $queue->getFirstItem();
+echo 'second item: ' . $item . "\n";
+if ($queue->save()) echo "\nsaved.\n";
+echo json_encode($queue->getItems());
+echo "\n";
+$items = $queue->getItems();
+$items[] = new Point(4,3);
+$items[] = new Point(5,3);
+$items[] = new Point(1,1);
+$queue->setItems($items);
+$queue->add(new Point(42,222));
+if ($queue->save()) echo "\nsaved.\n";
+echo "test 2 done\n\n";
+
+$queue->destroy();
+
+?>
