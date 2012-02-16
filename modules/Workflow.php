aaa68ebf8e08d5c8516392c4c0c208c3fc59f5f2
chadgh <chadgh@gmail.com>
2012-02-16 13:52:49 -0700

    added workflow module

---
+++ modules/Workflow.php
@@ -0,0 +1,44 @@
+<?php
+
+class Workflow extends Queue {
+	private $currentIndex;
+	private $workflow;
+
+	// Constructor: loads the specified queue
+	//   $saveOnChanges: specifies whether the queue should be saved to the
+	//     data source after each change or not (default = false)
+	// --------------------------------------------------
+
+	public function __construct($workflow, $index = 0) {
+		parent::__construct();
+		$this->workflow = explode(',', $workflow);
+		$this->setIndex($index);
+	}
+
+	public function setIndex($input) {
+		$this->currentIndex = $input;
+		$this->setItems(array_splice($this->workflow, $this->currentIndex));
+	}
+
+	public function getIndex() {
+		return $this->currentIndex;
+	}
+	
+
+	static public function register($action, $function) {
+		return parent::register($action.'_workflow', $function);
+	}
+
+	// next: call callback function for next item.
+	// --------------------------------------------------
+	public function next($params) {
+		$rtn = false;
+		if (array_key_exists('callback_workflow', self::$functions) && self::$functions['callback_workflow'] != null) {
+			$currentAction = $this->getFirstItem();
+			$this->currentIndex++;
+			$rtn = call_user_func(self::$functions['callback_workflow'], $params, $currentAction);
+		}
+		return $rtn;
+	}
+}
+?>
