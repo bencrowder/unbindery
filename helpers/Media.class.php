861573be554acf78e0996043e65d10e2360ca558
bencrowder <ben.crowder@gmail.com>
2012-10-23 09:15:25 -0600

    Wrote Media module

---
+++ helpers/Media.class.php
@@ -0,0 +1,130 @@
+<?php
+
+class Media {
+
+	// --------------------------------------------------
+	// Media::moveFiles
+	// $files = array of filenames (full paths)
+	// $destDir = destination directory (path relative to htdocs/media)
+
+	static public function moveFiles($files, $destDir) {
+		$sysPath = Settings::getProtected('sys_path');
+		$targetDir = "$sysPath/htdocs/media/$destDir";	// the full path to the destination
+
+		// Make sure there are files to move
+		if (empty($files)) return false;
+
+		// Make sure the destination directory exists and is a directory
+		if (!$destDir || !file_exists($targetDir) || !is_dir($targetDir)) return false;
+
+		// Make sure it's writable
+		if (!is_writable($targetDir)) return false;
+			
+		// Go through the files and move them
+		foreach ($files as $file) {
+			// Make sure it's a file
+			if (is_file($file)) {
+				// Get the base filename
+				$basename = basename($file);
+
+				error_log("Moving $file to $targetDir/$basename");
+
+				// Move it
+				$status = rename($file, "$targetDir/$basename");
+				if (!$status) return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	// --------------------------------------------------
+	// Media::moveFilesForProject
+	// Moves files from the project's temp dir into the final location
+	// $project = project object
+	// $files = array of filenames (relative paths), defaults to everything in $sysPath/htdocs/media/temp/$slug
+
+	static public function moveFilesForProject($project, $files = array()) {
+		// Get slug, type
+
+		$tempDir = "$sysPath/htdocs/media/temp/{$project->slug}";
+
+		// Set up the target dir
+		if ($project->type == 'system') {
+			$targetDir = "$sysPath/htdocs/media/projects/{$project->slug}";
+		} else if ($project->type == 'user') {
+			$targetDir = "$sysPath/htdocs/media/users/{$project->owner}/{$project->slug}";
+		}
+
+		// Make sure the directory exists, and if it doesn't, create it
+		if (!file_exists($targetDir)) {
+			mkdir($targetDir, 0777, true);
+		}
+
+		error_log("Moving files for project {$project->slug}, array = " . print_r($files, true));
+		error_log("Temp dir: $tempDir");
+		error_log("Target dir: $targetDir");
+
+		// Set up the array
+		$filesToMove = array();
+
+		// If the files array is empty
+		if (empty($files)) {
+			// Get everything in the temp dir
+			if ($handle = opendir($tempDir)) {
+				while (($file = readdir($handle)) !== false) {
+					// If it's a file, add it to the array of files to move
+					if (is_file("$tempDir/$file")) {
+						array_push($filesToMove, "$tempDir/$file");
+					}
+				}
+			}
+		} else {
+			// Loop through the files array
+			foreach ($files as $file) {
+				array_push($filesToMove, "$tempDir/$file");
+			}
+		}
+
+		error_log("Files to move: " . print_r($filesToMove, true));
+
+		// Finally, move the files
+		Media::moveFiles($filesToMove, $targetDir);
+
+		return true;
+	}
+
+
+	// --------------------------------------------------
+	// Media::moveUploadedFilesToTempDir()
+	// Does what the name says it does
+
+	static public function moveUploadedFilesToTempDir() {
+		if (!empty($_FILES)) {
+			// Get the filename and project slug
+			$tempFile = $_FILES['items']['tmp_name'];
+			$projectSlug = Utils::REQUEST('project');
+
+			// Set the target directory
+			$targetDir = "$sysPath/htdocs/media/temp/$projectSlug";
+
+			// If the directory doesn't exist, create it
+			if (!file_exists($targetDir)) {
+				mkdir($targetDir, 0777, true);
+			}
+
+			// And the target filename with path
+			$targetFile = $targetDir . $_FILES['items']['name'];
+			
+			// Move the file
+			move_uploaded_file($tempFile, $targetFile);
+
+			// Strip the targetDir first since we only care about the filename
+			// And echo the filename back so the JavaScript can know it's done
+			echo str_replace($targetDir, '', $targetFile);
+		}
+	}
+}
+
+?>
